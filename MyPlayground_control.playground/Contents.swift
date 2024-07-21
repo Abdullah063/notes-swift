34242191e83bdcbb45e4ddcc1537d8fe87aab1b9
Abdullah Altun <alttun0@gmail.com>
2024-07-21 19:25:19 +0300

    notes

--- MyPlayground_control.playground/Contents.swift
+++ MyPlayground_control.playground/Contents.swift
@@ -1,12 +1,33 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Değişkenler (Variables)
+var number = 12               // Bir tamsayı (integer) değişkeni
+var text = "hello"            // Bir metin (string) değişkeni
 
+// Kullanıcı Adı (User Name)
+var userName = "abdullah"     // Bir metin (string) değişkeni
 
-var age = 23
+// Selamlama Mesajı (Greeting Message)
+var greeting = "Hello, playground" // Bir metin (string) değişkeni
+print(5 * 4)                  // Basit bir aritmetik işlem ve sonucu yazdırma
+var num = 45 * 4              // Aritmetik işlemin sonucu bir değişkene atanır
 
-if age<30{
-    print("you are young ")
-}else{
-    print("you are old !!")
-}
+// Sabitler (Constants)
+let pi = 3.14                 // Pi sayısı (constant) - Değiştirilemez
+
+// String Türü Değişken (String Type Variable)
+var myString: String = "my string "
+
+// Diziler (Arrays)
+var array = ["kemal", "derya", "mehmet"] // String türünde elemanları olan dizi
+
+// Farklı türlerde elemanları olan dizi (Array with Any type elements)
+var arrayAny = ["metin", "ali", 8, true] as [Any]
+
+// Setler (Sets) - Elemanların sırası belli değildir ve her eleman yalnızca bir kez bulunur
+var mySet: Set = [1, 2, 3]
+print(mySet)
+
+// Sözlükler (Dictionaries) - Anahtar ve değer çiftleri
+var surname = ["mehmet": "altun", "derya": "der"]
+print(surname)
